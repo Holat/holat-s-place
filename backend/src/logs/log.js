589629013ecac117d8bac7ca4e83934ef98c2d26
Holat <105239299+Holat@users.noreply.github.com>
2024-07-06 20:51:34 +0100

    payment flow update

--- backend/src/logs/log.js
+++ backend/src/logs/log.js
@@ -1,7 +1,9 @@
-export default function log(file, data){
-    if (!file && !data ) return;
-    fs.appendFile(file, data, function (err) {
-        if (err) throw err;
-        console.log('The "data to append" was appended to file!');
-    });
+import fs from "fs";
+
+export default function log(file, data) {
+  if (!file && !data) return;
+  fs.appendFile(file, data, function (err) {
+    if (err) throw err;
+    console.log('The "data to append" was appended to file!');
+  });
 }
