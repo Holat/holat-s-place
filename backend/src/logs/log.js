4336f5aa2eb0d159aea31d8a359c0766d4682059
Holat <105239299+Holat@users.noreply.github.com>
2024-07-08 07:26:27 +0100

    log

--- backend/src/logs/log.js
+++ backend/src/logs/log.js
@@ -2,8 +2,15 @@ import fs from "fs";
 
 export default function log(file, data) {
   if (!file && !data) return;
-  fs.appendFile(file, data, function (err) {
-    if (err) throw err;
-    console.log('The "data to append" was appended to file!');
-  });
+  fs.appendFile(
+    file,
+    `${data}\n=============================================================================================`,
+    function (err) {
+      if (err) {
+        console.log("Error appending");
+        return;
+      }
+      console.log('The "data to append" was appended to file!');
+    }
+  );
 }
