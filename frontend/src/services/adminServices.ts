6362d58d63c99e8e092b3c70b88efb4febe490d3
Holat <105239299+Holat@users.noreply.github.com>
2024-08-23 13:01:03 +0100

    ghjk

--- frontend/src/services/adminServices.ts
+++ frontend/src/services/adminServices.ts
@@ -9,3 +9,10 @@ export const getOrderDetails = async () => {
   const { data } = await axios.get("/adminApi/revDetails");
   return data;
 };
+
+export const getCountries = async () => {
+  const { data } = await axios.get(
+    "https://restcountries.com/v3.1/all?fields=name"
+  );
+  return data ? data.map((country) => country.name.common) : [];
+};
