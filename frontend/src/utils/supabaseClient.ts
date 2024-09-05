1c59c9aabda500227d7bbeb64026d0981ec268cc
Holat <105239299+Holat@users.noreply.github.com>
2024-09-05 11:47:22 +0100

    update

--- frontend/src/utils/supabaseClient.ts
+++ frontend/src/utils/supabaseClient.ts
@@ -1,7 +1,5 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://drmakcnqbxnllugyswjm.supabase.co";
-const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRybWFrY25xYnhubGx1Z3lzd2ptIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjEyMDQ3NTksImV4cCI6MjAzNjc4MDc1OX0.CPrzfWbfggShXw7-MJpdB1jd1S1cQvQ7jBgVgEvREUg";
-
+const supabaseUrl = SUPABASE_URL || "";
+const supabaseKey = process.env.SUPABASE_ANON_KEY || "";
 export const supabase = createClient(supabaseUrl, supabaseKey);
