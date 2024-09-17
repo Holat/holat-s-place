8232850e2016b31f703fe1098c3ae066c990f234
Holat <105239299+Holat@users.noreply.github.com>
2024-09-17 10:24:40 +0100

    assets relocation

---
+++ frontend/src/assets/icons/index.ts
@@ -0,0 +1,40 @@
+import addIcon from "./add.svg";
+import addIIcon from "./addI.svg";
+import adminIcon from "./admin.svg";
+import cartIcon from "./cart.svg";
+import closeIcon from "./close.svg";
+import deleteIcon from "./delete.svg";
+import editIcon from "./edit.svg";
+import filterIcon from "./filter..svg";
+import homeIcon from "./home.svg";
+import lensIcon from "./lens.svg";
+import locateIcon from "./locate.svg";
+import loginIcon from "./login.svg";
+import logoutIcon from "./logout.svg";
+import menuIcon from "./menu.svg";
+import notFoundIcon from "./notFound.svg";
+import orderIcon from "./order.svg";
+import profileIcon from "./profile.svg";
+import settingIcon from "./setting.svg";
+
+
+export {
+    addIcon,
+    addIIcon,
+    adminIcon,
+    cartIcon,
+    closeIcon,
+    deleteIcon,
+    editIcon,
+    filterIcon,
+    homeIcon,
+    lensIcon,
+    locateIcon,
+    loginIcon,
+    logoutIcon,
+    menuIcon,
+    notFoundIcon,
+    orderIcon,
+    profileIcon,
+    settingIcon
+}
