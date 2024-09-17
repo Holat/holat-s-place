48c4ff143b93e7e68cc75c580820c909bf7060de
Holat <105239299+Holat@users.noreply.github.com>
2024-09-17 17:07:57 +0100

    filter init

---
+++ frontend/src/hooks/useOrderFilter.ts
@@ -0,0 +1,47 @@
+import { OrderHistoryType } from  "../../types/types";
+import { useState, useEffect } from 'react';
+
+const useOrderFilter = (orders: OrderHistoryType[]) => {
+  const [selectedStatus, setSelectedStatus] = useState(null);
+  const [startDate, setStartDate] = useState(null);
+  const [endDate, setEndDate] = useState(null);
+  const [filteredOrders, setFilteredOrders] = useState([]);
+
+  useEffect(() => {
+    const filterOrders = () => {
+      const filtered = orders.filter((order) => {
+        const orderDate = new Date(order.createdAt);
+        if (startDate === null && endDate === null) {
+          return selectedStatus === null || order.status === selectedStatus;
+        }
+
+        if (selectedStatus === null) {
+          return (
+            (startDate === null || orderDate >= startDate) &&
+            (endDate === null || orderDate <= endDate)
+          );
+        }
+
+        return (
+          order.status === selectedStatus &&
+          (startDate === null || orderDate >= startDate) &&
+          (endDate === null || orderDate <= endDate)
+        );
+      });
+      setFilteredOrders(filtered);
+    };
+    filterOrders();    
+  }, [orders, selectedStatus, startDate, endDate]);
+
+  return {
+    selectedStatus,
+    setSelectedStatus,
+    startDate,
+    setStartDate,
+    endDate,
+    setEndDate,
+    filteredOrders,
+  };
+};
+
+export default useOrderFilter;
--- frontend/src/pages/Admin/OrderTable.tsx
+++ frontend/src/pages/Admin/OrderTable.tsx
@@ -1,6 +1,8 @@
 import { OrderHistoryType } from "../../types/types";
 import { formatDateToDDMMYYYY } from "../../utils/adminForm";
 import { Price, Title } from "../../components";
+import { refreshIcon } from "../../icons"
+import useOrderFilter from "../../hooks/useOrderFilter";
 
 const lightTheme = {
   accentV: "#ffc107",
@@ -9,12 +11,44 @@ const lightTheme = {
   failed: "#dc3545",
 };
 
-const OrderTable = ({ orders }: { orders?: OrderHistoryType[] }) => {
+const OrderTable = ({ orders, refresh }: { orders?: OrderHistoryType[], refresh: () => void }) => {
+  const {  
+    selectedStatus,
+    setSelectedStatus,
+    startDate,
+    setStartDate,
+    endDate,
+    setEndDate,
+    filteredOrders
+  } = useOrderFilter(order);
+
   return (
     <div className="order-status-table">
       <div className="search-bar">
         <Title title="Orders" fontWeight={600} fontSize="20px" />
-        <input type="text" placeholder="Search" />
+        <div className="searchBox">
+          <button onClick={refresh}>
+            <img src={refreshIcon} alt="refresg" />
+          </button>
+          <input type="text" placeholder="Search" />
+          <div className="filter">
+            {
+              order && 
+              (
+                <>
+                  <input type="date" value={ startDate ? startDate.toISOString().split("T")[0] : ''} onChange={(e) => setStartDate(new Date(e.target.value))}/>
+                  <input type="date" value={ endDate ? endDate.toISOString().split("T")[0] : ''} onChange={(e) => setEndDate(new Date(e.target.value))}/>
+                  <select name="" id="" value={selectedStatus} onChange={(e) => setSelectedStatus(e.target.value)}>
+                    <option value="">All Statuses</option>
+                    <option value="PAID">Paid</option>
+                    <option value="FAILED">Failed</option>
+                    <option value="CANCELLED">Cancelled</option>
+                  </select>
+                </>
+              )
+            }
+          </div>
+        </div>
       </div>
       <table>
         <tbody>
